4ccf0490401d735e5c2a3eb405613f81b84bd445
sendusyourbones <beckee.roos@gmail.com>
2023-02-24 15:15:26 -0800

    Create js file with array of questions/answers

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+const questions = [
+    {
+        question: 'Which option would you use to grab the element with the ID "cats"?',
+        answerOptions: [
+            'window.querySelector("#cats");',
+            'document.idSelector("cats");',
+            'document.querySelector("#cats");',
+            'document.elementSelector("#cats");'
+        ],
+        correctAnswer: 2
+    },
+    {
+        question: 'Which event type would you use if you wanted to take action when a user presses a key on the keyboard?',
+        answerOptions: [
+            'keypush',
+            'keydown',
+            'keyup',
+            'type'
+        ],
+        correctAnswer: 1
+    },
+    {
+        question: 'The second parameter for setInterval() uses which measurement of time?',
+        answerOptions: [
+            'seconds',
+            'microseconds',
+            'minutes',
+            'milliseconds',
+        ],
+        correctAnswer: 3
+    },
+    {
+        question: 'Which of the following statements about arrays is false?',
+        answerOptions: [
+            'Two arrays that contain the same elements in the same order are equal to each other',
+            'Arrays have a .forEach() method that can be used to execute the same code on all elements in the array',
+            'Arrays begin at index 0',
+            'If you try to access an element using an index that does not exist in the array, you will get undefined'
+        ],
+        correctAnswer: 0
+    },
+    {
+        question: 'Given an object with the name "dog" and a key "name", how would you print the value of "name" to the console?',
+        answerOptions: [
+            'console.log(dog#name);',
+            'console.log(dog.name);',
+            'console.log(dog:name);',
+            'console.log(dog>name);'
+        ],
+        correctAnswer: 1
+    }
+];

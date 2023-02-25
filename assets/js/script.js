f70c456fff1e4e09021b308f4229192ee4b5cba7
sendusyourbones <beckee.roos@gmail.com>
2023-02-24 17:56:42 -0800

    Change inputs to buttons. Add event listener for verifying answer.

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,80 +21,87 @@ setTime();
 const questions = [
     {
         question: 'Which option would you use to grab the element with the ID "cats"?',
-        answerOptions: [
+        ansOptions: [
             'window.querySelector("#cats");',
             'document.idSelector("cats");',
             'document.querySelector("#cats");',
             'document.elementSelector("#cats");'
         ],
-        inputName: 'el-by-id',
-        correctAnswer: 2
+        ansIndex: 2
     },
     {
         question: 'Which event type would you use if you wanted to take action when a user presses a key on the keyboard?',
-        answerOptions: [
+        ansOptions: [
             'keypush',
             'keydown',
             'keyup',
             'type'
         ],
-        inputName: 'press-key',
-        correctAnswer: 1
+        ansIndex: 1
     },
     {
         question: 'The second parameter for setInterval() uses which measurement of time?',
-        answerOptions: [
+        ansOptions: [
             'seconds',
             'microseconds',
             'minutes',
             'milliseconds',
         ],
-        inputName: 'interval',
-        correctAnswer: 3
+        ansIndex: 3
     },
     {
         question: 'Which of the following statements about arrays is false?',
-        answerOptions: [
+        ansOptions: [
             'Two arrays that contain the same elements in the same order are equal to each other',
             'Arrays have a .forEach() method that can be used to execute the same code on all elements in the array',
             'Arrays begin at index 0',
             'If you try to access an element using an index that does not exist in the array, you will get undefined'
         ],
-        inputName: 'arrays',
-        correctAnswer: 0
+        ansIndex: 0
     },
     {
         question: 'Given an object with the name "dog" and a key "name", how would you print the value of "name" to the console?',
-        answerOptions: [
+        ansOptions: [
             'console.log(dog#name);',
             'console.log(dog.name);',
             'console.log(dog:name);',
             'console.log(dog>name);'
         ],
-        inputName: 'key-value',
-        correctAnswer: 1
+        ansIndex: 1
     }
 ];
 
 const questionEl = document.getElementById('question');
-const answersEl = document.getElementById('answers');
+const answersDiv = document.getElementById('answers');
+let questionIndex = 0;
 
 // Build an individual question with answer choices and append to the answers element
 function showQuestion() {
-    let questionIndex = 0;
     questionEl.textContent = questions[questionIndex].question;
 
     for (let i = 0; i < 4; i++) {
         let answerInput = document.createElement('input');
-        answerInput.setAttribute('type', 'radio');
-        answerInput.setAttribute('name', questions[questionIndex].inputName);
-        answerInput.setAttribute('id', `option${i}`);
-        answersEl.appendChild(answerInput);
-        let answerLabel = document.createElement('label');
-        answerLabel.textContent = questions[questionIndex].answerOptions[i];
-        answerLabel.setAttribute('for', `option${i}`);
-        answersEl.appendChild(answerLabel);
+        answerInput.setAttribute('type', 'button');
+        answerInput.setAttribute('value', questions[questionIndex].ansOptions[i]);
+        answersDiv.appendChild(answerInput);
     }
 }
 
 showQuestion();
+
+// Event listener for selecting an answer option
+const questionResult = document.getElementById('question-result');
+const answerEls = document.querySelectorAll('input');
+
+answerEls.forEach(element => {
+    element.addEventListener("click", function() {
+        let chosenAns = element.value;
+        let correctAnsIndex = questions[questionIndex].ansIndex;
+        let correctAns = questions[questionIndex].ansOptions[correctAnsIndex];
+        if (chosenAns === correctAns) {
+            questionResult.textContent = 'Correct!';
+        } else {
+            questionResult.textContent = 'Incorrect :(';
+        }
+    });
+});
